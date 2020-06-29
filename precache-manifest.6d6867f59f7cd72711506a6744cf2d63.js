88fc2619d734808be7e24d2323f0200db8c4894c
Juanmaponce <juanmaponce333@gmail.com>
2020-06-29 17:19:00 -0300

    Updates

---
+++ precache-manifest.6d6867f59f7cd72711506a6744cf2d63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1b8f2c95be99ae568bc2b64b890067",
+    "url": "/jams-jmp/index.html"
+  },
+  {
+    "revision": "c41ccedbf16dc23d8351",
+    "url": "/jams-jmp/static/css/main.1f8d133e.chunk.css"
+  },
+  {
+    "revision": "7a59cd78ea1781494612",
+    "url": "/jams-jmp/static/js/2.7da80ee2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jams-jmp/static/js/2.7da80ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41ccedbf16dc23d8351",
+    "url": "/jams-jmp/static/js/main.2ea9cea1.chunk.js"
+  },
+  {
+    "revision": "cadcf7f7cbbb47ef4d80",
+    "url": "/jams-jmp/static/js/runtime-main.d6d26d5e.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jams-jmp/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
